ec1a9df83c691eaea7353f89aee43579b9f45883
schommi <49872973+schommi@users.noreply.github.com>
2019-09-28 09:18:54 +0200

    Update extract_payload.py

--- 010-Flareon6 Bmphide/extract_payload.py
+++ 010-Flareon6 Bmphide/extract_payload.py
@@ -1,1 +1,14 @@
+from PIL import Image
 
+encrypted_data = []
+with Image.open("image3.bmp", "r") as image:
+  for column in range(image.width):
+    for row in range(image.height):
+      red, green, blue = image.getpixel((column, row))
+
+      original_data = (red & 0x7) | ((green & 0x7) << 3) | ((blue & 0x3) << 6)
+      encrypted_data.append(original_data)
+
+binary_format = bytearray(encrypted_data)
+with open("encrypted-data.bin", "wb") as encrypted:
+  encrypted.write(binary_format)
